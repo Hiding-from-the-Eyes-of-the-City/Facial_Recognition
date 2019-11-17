6da490c9b2fe712e8fb85a82b5039297ae000c0a
Dhvanil Shah <dhv_19@yahoo.com>
2019-11-17 14:42:58 -0500

    added error checking for the file imports

--- face_detec_mtcnn2.py
+++ face_detec_mtcnn2.py
@@ -7,7 +7,6 @@ import time
 
 detector = MTCNN()
 
-
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(1)
 
@@ -20,32 +19,38 @@ while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
     # Resize frame of video to 1/4 size for faster face recognition processing
-    #small_frame = frame
+    # small_frame = frame
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
 
-    np.load.__defaults__=(None, True, True, 'ASCII')
-    
+    np.load.__defaults__ = (None, True, True, 'ASCII')
+
     while not path.exists('encoding.npy'):
         time.sleep(1)
     while True:
-        try:
-            known_face_encodings = np.load('encoding.npy')
+        while True:
+            try:
+                known_face_encodings = np.load('encoding.npy')
+                break
+            except:
+                continue
+
+        while not path.exists('names.npy'):
+            time.sleep(1)
+        while True:
+            try:
+                known_face_names = np.load('names.npy')
+                break
+            except:
+                continue
+        if len(known_face_encodings) == len(known_face_names):
             break
-        except:
+        else:
             continue
-    
-    while not path.exists('names.npy'):
-        time.sleep(1)
-    while True:
-        try:
-            known_face_names = np.load('names.npy')
-            break
-        except:
-            continue
-   #print(known_face_encodings.shape)
-    #print(known_face_names)
+
+# print(known_face_encodings.shape)
+# print(known_face_names)
 
     # Only process every other frame of video to save time
     if process_this_frame:
@@ -87,21 +92,22 @@ while True:
         right *= 4
         bottom *= 4
         left *= 4
-        
+
         bounding_box = result_mtcnn[0]['box']
         cv2.rectangle(frame,
-                          (bounding_box[0]*4, bounding_box[1]*4),
-                          (bounding_box[0]*4+bounding_box[2]*4, bounding_box[1]*4 + bounding_box[3]*4),
-                          (0,155,255),
-                          2)
+                      (bounding_box[0] * 4, bounding_box[1] * 4),
+                      (bounding_box[0] * 4 + bounding_box[2] * 4, bounding_box[1] * 4 + bounding_box[3] * 4),
+                      (0, 155, 255),
+                      2)
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-    #cv2.imshow('SMALL_FRAME', small_frame)
-    cv2.imshow('Video', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        # cv2.imshow('SMALL_FRAME', small_frame)
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 video_capture.release()
 cv2.destroyAllWindows()
